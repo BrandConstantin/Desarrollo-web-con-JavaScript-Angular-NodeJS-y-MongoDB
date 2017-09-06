e2c9a9eba9f52f4bc5ceac04f5617a88879ff3ae
BrandConstantin <brand_constantin@hotmail.com>
2017-09-06 12:58:32 +0200

    curso completado 26%

--- curso-mean/routes/artist.js
+++ curso-mean/routes/artist.js
@@ -8,8 +8,17 @@ var ArtistController = require('../controllers/artist');
 var api = express.Router();
 //cargar el middleware para restringir el acceso a usu no identificados
 var md_auth = require('../middlewares/authenticated');
+//método de cargar las imágenes
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/artists' });
 
 //crear una ruta de prueba
-api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
+api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
+api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
+api.delete('/artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist);
+api.post('/upload-image-artist/:id', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
+api.get('/get-image-artist/:imageFile', ArtistController.getImageFile);
 
 module.exports = api;
